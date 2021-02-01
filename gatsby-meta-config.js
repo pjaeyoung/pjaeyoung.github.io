3cac8615d64e9db3a13118ca3281a25833fd1646
pjaeyoung <saiei2484@gmail.com>
2021-02-01 18:18:21 +0900

    init commit

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,21 +1,21 @@
 module.exports = {
   title: `Bee starter`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
+  author: `dodo`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
+    github: `https://github.com/pjaeyoung`, // Your GitHub account
+    twitter: ``,
+    medium: ``,
+    facebook: ``,
+    linkedin: ``,
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    disqusShortName: 'dodo', // Your disqus-short-name. check disqus.com.
+    utterances: 'pjaeyoung/dodo-blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
@@ -26,5 +26,5 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'G-8NSQEPGEQJj', // Add your google analytics tranking ID
 }
